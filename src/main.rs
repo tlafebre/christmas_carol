be320688a767095838e9c15f751dcb803c4e639d
Tjeerd Lafebre <tjeerd.lafebre@gmail.com>
2019-06-27 08:24:43 +0200

    refactored after code review

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,48 @@
 fn main() {
     for iteration in 1..13 {
-        let nth_time = nth(iteration).0;
-        let mut presents_list = Vec::new();
 
-        for i in 1..1 + iteration {
-            presents_list.insert(0, nth(i).1)
+        println!("On the {} day of Christmas,\nMy true love sent to me", day(iteration));
+
+        let presents = (1..=iteration).rev().map(present);
+        for present in presents {
+            println!("{}", present);
         }
-        
-        let presents = presents_list.join("\n");
-
-        println!("On the {} day of Christmas,\nMy true love sent to me\n{}\n", nth_time, presents);
+        println!(""); 
     }
 }
 
-fn nth(n: u8) -> (String, String) {
+fn day(n: u8) -> &'static str {
     match n {
-        1 => (String::from("First"), String::from("A partridge in a pear tree")),
-        2 => (String::from("Second"), String::from("Two turtle-doves and")),
-        3 => (String::from("Third"), String::from("Three French hens,")),
-        4 => (String::from("Fourth"), String::from("Four calling birds,")),
-        5 => (String::from("Fifth"), String::from("Five golden rings.")),
-        6 => (String::from("Sixth"), String::from("Six geese a-laying,")),
-        7 => (String::from("Seventh"), String::from("Seven swans a-swimming,")),
-        8 => (String::from("Eighth"), String::from("Eight maids a-milking,")),
-        9 => (String::from("Ninth"), String::from("Nine ladies dancing,")),
-        10 => (String::from("Tenth"), String::from("Ten lords a-leaping,")),
-        11 => (String::from("Eleventh"), String::from("Eleven pipers piping,")),
-        12 => (String::from("Twelfth"), String::from("Twelve drummers drummings,")),
-        _ => (String::from(""), String::from("")),
+        1 => "First",
+        2 => "Second",
+        3 => "Third",
+        4 => "Fourth",
+        5 => "Fifth",
+        6 => "Sixth",
+        7 => "Seventh",
+        8 => "Eighth",
+        9 => "Ninth",
+        10 => "Tenth",
+        11 => "Eleventh",
+        12 => "Twelfth",
+        _ => "",
+    }
+}
+
+fn present(n: u8) -> &'static str {
+    match n {
+        1 => "A partridge in a pear tree",
+        2 => "Two turtle-doves and",
+        3 => "Three French hens,", 
+        4 => "Four calling birds,",
+        5 => "Five golden rings.",
+        6 => "Six geese a-laying,",
+        7 => "Seven swans a-swimming,",
+        8 => "Eight maids a-milking,",
+        9 => "Nine ladies dancing,",
+        10 => "Ten lords a-leaping,",
+        11 => "Eleven pipers piping,",
+        12 => "Twelve drummers drummings,",
+        _ => "",
     }
 }
